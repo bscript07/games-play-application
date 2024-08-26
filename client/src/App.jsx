b9f4add9251c8f0727d9b3797b785d34d1dc5e08
Brayan Markov <brayan_markov@yahoo.com>
2024-08-26 18:04:29 +0300

    Add persisted state hook

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,5 @@
-import { useState } from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 
-import * as authService from './services/authService';
 import { AuthProvider } from './context/authContext';
 import Path from './paths';
 
@@ -15,58 +13,23 @@ import Register from './components/register/Register';
 import GameDetails from './components/game-details/GameDetails';
 
 function App() {
-    const navigate = useNavigate();
-    const [auth, setAuth] = useState(() => {
-        localStorage.removeItem('accessToken');
-
-        return {};
-    });
-
-    const loginSubmitHandler = async (values) => {
-       const result = await authService.login(values.email, values.password);
-
-       setAuth(result);
-       localStorage.setItem('accessToken', result.accessToken);
-       navigate(Path.Home);
-    }
-
-    const registerSubmitHandler = async (values) => {
-        const result = await authService.register(values.email, values.password);
-
-        setAuth(result);
-        localStorage.setItem('accessToken', result.accessToken);
-        navigate(Path.Home);
-    }
-
-    const logoutHandler = () => {
-        setAuth({});
-        localStorage.removeItem('accessToken');
-    }
-
-    const values = {
-        loginSubmitHandler,
-        registerSubmitHandler,
-        logoutHandler,
-        username: auth.username || auth.email,
-        email: auth.email,
-        isAuthenticated: !!auth.accessToken,
-    }
 
     return (
-        <AuthProvider value={values}>
-        <div id="box">
-            <Header />
+        <AuthProvider>
+            <div id="box">
+                <Header />
 
-            <Routes>
-                <Route path={Path.Home} element={<Home />} />
-                <Route path={Path.Games} element={<GameList />} />
-                <Route path={Path.CreateGame} element={<GameCreate />} />
-                <Route path={Path.Login} element={<Login />} />
-                <Route path={Path.Register} element={<Register  />} />
-                <Route path={Path.GameDetails} element={<GameDetails /> } />
-                <Route path={Path.Logout} element={<Logout />} />
-            </Routes>
-        </div>
+                <Routes>
+                    <Route path={Path.Home} element={<Home />} />
+                    <Route path={Path.Games} element={<GameList />} />
+                    <Route path={Path.CreateGame} element={<GameCreate />} />
+                    <Route path={Path.Login} element={<Login />} />
+                    <Route path={Path.Register} element={<Register />} />
+                    <Route path={Path.GameDetails} element={<GameDetails />} />
+                    <Route path={Path.Logout} element={<Logout />} />
+                </Routes>
+
+            </div>
         </AuthProvider>
     )
 }
--- client/src/context/authContext.jsx
+++ client/src/context/authContext.jsx
@@ -1,11 +1,49 @@
 import { createContext } from "react";
+import { useNavigate } from "react-router-dom";
+
+import Path from '../paths';
+import * as authService from '../services/authService';
+import usePersistedState from "../hooks/usePersistedState";
 
 const AuthContext = createContext();
 
-export const AuthProvider = ({children, value}) => {
+export const AuthProvider = ({children}) => {
+
+    const navigate = useNavigate();
+    const [auth, setAuth] = usePersistedState('auth', {});
+
+    const loginSubmitHandler = async (values) => {
+       const result = await authService.login(values.email, values.password);
+
+       setAuth(result);
+       localStorage.setItem('accessToken', result.accessToken);
+       navigate(Path.Home);
+    }
+
+    const registerSubmitHandler = async (values) => {
+        const result = await authService.register(values.email, values.password);
+
+        setAuth(result);
+        localStorage.setItem('accessToken', result.accessToken);
+        navigate(Path.Home);
+    }
+
+    const logoutHandler = () => {
+        setAuth({});
+        localStorage.removeItem('accessToken');
+    }
+
+    const values = {
+        loginSubmitHandler,
+        registerSubmitHandler,
+        logoutHandler,
+        username: auth.username || auth.email,
+        email: auth.email,
+        isAuthenticated: !!auth.accessToken,
+    }
 
     return (
-        <AuthContext.Provider value={value}>
+        <AuthContext.Provider value={values}>
             {children}
         </AuthContext.Provider>
     )
